51d04f70b1694af742dd5e320f4a5c52e85aa014
gbarbutov <georgi.barbutov97@gmail.com>
2021-05-23 22:53:04 +0300

    Added dendrogram visualization

--- zoo.py
+++ zoo.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import dendrogram
+from scipy.cluster import hierarchy
 
 def import_data():
     return  pd.read_csv(os.getcwd() + "/data.csv", header=None)
@@ -7,15 +10,15 @@ def import_data():
 #calculates distance between 2 entries in data_values
 def calculate_distance(animal1, animal2):
     distance = 0
-    for i in range(1, 17):
+    for i in range(1, len(animal1) - 1):
         if animal1[i] != animal2[i]:
             distance += 1
     return distance  
 
 def complete_linkage(cluster1, cluster2, data_values):
     max = -1
-    for i in cluster1:
-        for j in cluster2:
+    for i in cluster1[1:]:
+        for j in cluster2[1:]:
             distance = calculate_distance(data_values[i], data_values[j])
             if distance > max:
                 max = distance
@@ -23,8 +26,8 @@ def complete_linkage(cluster1, cluster2, data_values):
 
 def single_linkage(cluster1, cluster2, data_values):
     min = 10000
-    for i in cluster1:
-        for j in cluster2:
+    for i in cluster1[1:]:
+        for j in cluster2[1:]:
             distance = calculate_distance(data_values[i], data_values[j])
             if distance < min:
                 min = distance
@@ -32,10 +35,10 @@ def single_linkage(cluster1, cluster2, data_values):
 
 def average_linkage(cluster1, cluster2, data_values):
     sum = 0
-    for i in cluster1:
-        for j in cluster2:
+    for i in cluster1[1:]:
+        for j in cluster2[1:]:
             sum += calculate_distance(data_values[i], data_values[j])
-    return sum/len(cluster1)*len(cluster2)
+    return sum/(len(cluster1)*len(cluster2))
 
 #calculates distance_matrix based on the chosen linkage function
 def get_distance_matrix(data_values, clusters, linkage):
@@ -57,7 +60,7 @@ def get_distance_matrix(data_values, clusters, linkage):
 def create_clusters(data): 
     clusters = []
     for i in range(0, len(data)):
-        clusters.append([i])
+        clusters.append([i, i])
     return clusters
 
 #finds the closest clusters, min_i, min_j are the indexes in the clusters list
@@ -74,21 +77,29 @@ def minimum_distance(distance_matrix):
                 min_j = j
     return (min, min_i, min_j)
 
-#[[1], [2]] -> [[1, 2]]
-def combine_clusters(clusters, i, j):
+#[[1, 1], [2, 2]] -> [[101, 1, 2]], [3,3] - > [[102, 1, 2, 3]]
+def combine_clusters(clusters, i, j, last_cluster_index):
+    clusters[i].pop(0)
+    clusters[j].pop(0)
     clusters[i].extend(clusters[j])
+    clusters[i].insert(0, last_cluster_index)
     clusters.remove(clusters[j])
 
 def AGNES(linkage, max_clusters, data_values):
     clusters = create_clusters(data_values)
     distance_matrix = get_distance_matrix(data_values, clusters, linkage)
     cluster_count = len(clusters)
+    last_cluster_index = cluster_count
+    Z = []
     while cluster_count > max_clusters:
         distance, i, j = minimum_distance(distance_matrix)
-        combine_clusters(clusters, i, j)
+        Z.append([clusters[i][0], clusters[j][0], float(distance), len(clusters[i]) + len(clusters[j]) - 2])
+        combine_clusters(clusters, i, j, last_cluster_index)
+        last_cluster_index += 1
         distance_matrix = get_distance_matrix(data_values, clusters, linkage)
         cluster_count -= 1
-    return clusters
+    print(distance_matrix)
+    return Z
 
 #prints resulting clusters and calculates misclasification percent
 def print_results(clusters):
@@ -100,8 +111,8 @@ def print_results(clusters):
         for j in range(0, max_clusters):
             types[j] = 0
         for j in i:
-            animals.append((data_values[j][0], data_values[j][17]))
-            types[data_values[j][17] - 1] += 1
+            animals.append((data_values[j][0], data_values[j][len(data_values[j]) - 1]))
+            types[data_values[j][len(data_values[j]) - 1] - 1] += 1
         
         max_type = -1
         max_type_i = -1
@@ -119,8 +130,13 @@ def print_results(clusters):
 data = import_data()
 data_values = data.values
 
-linkage = "complete"
-max_clusters = 7
+linkage = "average"
+max_clusters = 1
 
-clusters = AGNES(linkage, max_clusters, data_values)
-print_results(clusters)
+Z = AGNES(linkage, max_clusters, data_values)
+print(len(Z))
+dn = hierarchy.dendrogram(Z)
+
+plt.show()
+
+#print_results(clusters)
